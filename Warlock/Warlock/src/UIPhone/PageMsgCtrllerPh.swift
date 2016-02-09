718196404eaeab23198a7ce834b51974bc208dff
卢乐颜 <frosen_lu@126.com>
2016-02-09 22:38:05 +0800

    read plist

--- Warlock/Warlock/src/UIPhone/PageMsgCtrllerPh.swift
+++ Warlock/Warlock/src/UIPhone/PageMsgCtrllerPh.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class PageMsgCtrllerPh: BasePageCtrllerPh {
+
+
     override func initPage() {
         print("init page msg")
         clearView()
         view.backgroundColor = UIColor.brownColor()
+
+        let tableView = UITableView(frame: view.bounds)
+        view.addSubview(tableView)
+
+        let plistPath = NSBundle.mainBundle().pathForResource("team", ofType: "plist", inDirectory: "res")
+        //获取属性列表文件中的全部数据
+        let listTeams: NSArray = NSArray(contentsOfFile: plistPath!)!
+
+        listTeams.map{print($0)}
+        
     }
 
     override func switchToolbarBtn() {
